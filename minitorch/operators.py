12fa0fdbc743c4ce733514d3693412eb167cfa6e
Thomason Zhou <thomason.zhou@uwaterloo.ca>
2024-12-18 14:50:20 -0700

    Redefine neg for float conversion

--- minitorch/operators.py
+++ minitorch/operators.py
@@ -50,7 +50,7 @@ def add(x: float, y: float) -> float:
 
 def neg(x: float) -> float:
     """Compute the negation of x."""
-    return -x
+    return -1.0 * x
 
 
 def lt(x: float, y: float) -> float:
