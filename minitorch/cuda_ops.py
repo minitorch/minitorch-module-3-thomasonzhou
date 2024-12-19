f2313e0db4f354614b86b35cd4dc1bcbcc9edcac
Thomason Zhou <thomason.zhou@uwaterloo.ca>
2024-12-18 20:15:26 -0700

    Change indexing

--- minitorch/cuda_ops.py
+++ minitorch/cuda_ops.py
@@ -325,7 +325,7 @@ def tensor_reduce(
         out_pos = cuda.blockIdx.x
         pos = cuda.threadIdx.x
 
-        to_index(pos, out_shape, out_index)
+        to_index(out_pos, out_shape, out_index)
         out_location = index_to_position(out_index, out_strides)
 
         if pos < a_shape[reduce_dim]:
